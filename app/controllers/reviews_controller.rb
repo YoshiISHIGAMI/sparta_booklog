9462f0d394953a9d4d8806151349ef9f7334fd17
Yoshi Ishigami <yoshi@ibf.sc>
2015-06-27 12:54:10 +0900

    レビュー表示を実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,22 @@ class ReviewsController < ApplicationController
   def new
     #book_id が1のオブジェクト
     @book = Book.find(params[:book_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(
+      book_id: params[:book_id], 
+      user_id: current_user.id,
+      body: review_params["body"]#ここができなかった
+    )
+    @review.save
+    redirect_to book_url(@review.book)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
   end
 end
